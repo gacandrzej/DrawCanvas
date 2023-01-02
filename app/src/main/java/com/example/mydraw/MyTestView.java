90d3470fa1e7343146a0518923102406d11ea3d4
Andrzej Gac <gac.andrzej@gmail.com>
2023-01-02 11:54:13 +0100

    v2

--- app/src/main/java/com/example/mydraw/MyTestView.java
+++ app/src/main/java/com/example/mydraw/MyTestView.java
@@ -2,11 +2,16 @@ package com.example.mydraw;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.RadialGradient;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.Typeface;
 import android.graphics.fonts.Font;
 import android.text.TextPaint;
@@ -25,11 +30,18 @@ public class MyTestView extends View {
     private Paint redPaint = null;
     private Paint greenPaint = null;
     private Paint yellowPaint = null;
-    private Paint pointPaint = null;
+    private Paint shadowPaint = null;
+    private Paint linearGradientPaint = null;
+    private Paint radialGradientPaint = null;
+    //
+    private RectF shadowBounds;
     private int heightPixels;
     private int widthPixels;
     private Path path;
-    private ArrayList<Point> points = null;
+    private Path polygonPath;
+    ArrayList<Point> points = null;
+    private int width;
+    private int height;
 
     public MyTestView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -39,84 +51,129 @@ public class MyTestView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-       myDraw(canvas);
+        initShader();
+        myDraw(canvas);
     }
+
+
+
     private void myDraw(Canvas canvas){
-        // draw point
         for(Point p: points){
-            canvas.drawPoint(p.x,p.y, pointPaint);
+            canvas.drawPoint(p.x,p.y,bluePaint);
         }
-        Log.v("1111",heightPixels + " "+ widthPixels);
-        canvas.drawCircle(widthPixels/2, heightPixels/2,100,greenPaint);
-        canvas.drawLine(20,30,widthPixels-20,30,greenPaint);
-        canvas.drawText("Idą Święta i Nowy Rok 2023!!!",20,heightPixels-200,redPaint);
-        canvas.drawTextOnPath("Mistrzostwa Świata w piłce nożnej wygra Argentyna", path,0,0,bluePaint);
-        canvas.drawRect(45,45,widthPixels-195,135,redPaint);
+        Log.v("1111",height + " "+ width);
+        Log.v("1111",getHeight() + " "+ getWidth());
+        // canvas.drawCircle(width/2, height/2,100,bluePaint);
+        canvas.drawLine(20,30,width-20,30,greenPaint);
+        canvas.drawText("Simple application 2023!!!",20,height-50,redPaint);
+        //  canvas.drawTextOnPath("Mistrzostwa Świata w piłce nożnej 2022",
+        //         path,0,0,redPaint);
+        canvas.drawRect(45,45,width-195,135,redPaint);
 
-        canvas.drawRect(50,50,widthPixels-200,130,yellowPaint);
+        canvas.drawRect(50,50,width-200,130,yellowPaint);
+        // linear gradient
+        canvas.drawRect(50,250,1000,400,linearGradientPaint);
 
+        canvas.drawCircle(getWidth()/2,getHeight()/2,getWidth()/3,radialGradientPaint);
 
+        shadowBounds = new RectF(50,0.75f*getHeight(),getWidth()-50,getHeight()-150);
+        canvas.drawOval(shadowBounds,shadowPaint);
+        //
+        canvas.drawPath(polygonPath,bluePaint);
     }
+    private void initShader() {
+        linearGradientPaint.setShader(new LinearGradient(50,250,1000,400,
+                Color.BLUE,
+                Color.GREEN,
+                Shader.TileMode.MIRROR
+        ));
+        radialGradientPaint.setShader( new RadialGradient(
+                getWidth()/2,
+                getHeight()/2,
+                getWidth()/3,
+                Color.RED,
+                Color.YELLOW,
+                Shader.TileMode.MIRROR
+        ));
+    }
+
+
     private void init(){
 
         points = new ArrayList<Point>();
 
-        heightPixels = getResources().getDisplayMetrics().heightPixels;
-        widthPixels = getResources().getDisplayMetrics().widthPixels;
-
+        //  heightPixels = getResources().getDisplayMetrics().heightPixels;
+        //   widthPixels = getResources().getDisplayMetrics().widthPixels;
         path = new Path();
-        path.addCircle(400,700,200, Path.Direction.CCW);
-
-        Typeface redTf =getResources().getFont(R.font.rage);
-        Typeface blueTf =getResources().getFont(R.font.chancery);
-        Typeface greenTf =getResources().getFont(R.font.freesctp);
-      // blue
+        path.addCircle(400,700,200, Path.Direction.CW);
+        //
+        polygonPath = new Path();
+        polygonPath.moveTo(100, 0);
+        polygonPath.lineTo(141, 71);
+        polygonPath.lineTo(217, 78);
+        polygonPath.lineTo(154, 125);
+        polygonPath.lineTo(166, 201);
+        polygonPath.lineTo(100, 170);
+        polygonPath.lineTo(34, 201);
+        polygonPath.lineTo(46, 125);
+        polygonPath.lineTo(0, 78);
+        polygonPath.lineTo(76, 71);
+        polygonPath.close();
+        Typeface tf =getResources().getFont(R.font.rage);
+        // blue
         bluePaint = new Paint();
-        bluePaint.setColor(   getResources().getColor( R.color.blue , null ));
-        bluePaint.setTextSize(55);
-        bluePaint.setStrokeWidth(3);
-        bluePaint.setTypeface(blueTf);
-        bluePaint.setStyle(Paint.Style.STROKE);
-      // red
+        bluePaint.setColor(Color.BLUE);
+        bluePaint.setTextSize(125);
+        bluePaint.setStrokeWidth(30);
+        bluePaint.setTypeface(tf);
+        bluePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        // red
         redPaint = new Paint();
-        redPaint.setColor(   getResources().getColor( R.color.red , null ));
+        redPaint.setColor(   getResources().getColor(
+                        R.color.red,
+                        null
+                )
+        );
         redPaint.setTextSize(85);
-        redPaint.setTypeface(redTf);
+        redPaint.setTypeface(tf);
         // green
         greenPaint = new Paint();
         greenPaint.setStrokeWidth(10);
         greenPaint.setColor(getResources().getColor(R.color.green,null));
         greenPaint.setTextSize(125);
-        greenPaint.setTypeface(greenTf);
+        greenPaint.setTypeface(tf);
 
         // yellow
         yellowPaint = new Paint();
         yellowPaint.setStrokeWidth(10);
         yellowPaint.setColor(getResources().getColor(R.color.yellow,null));
         yellowPaint.setTextSize(125);
-       // another
-        pointPaint = new Paint();
-        pointPaint.setStrokeWidth(50);
-        pointPaint.setColor(getResources().getColor(R.color.orange,null));
-        pointPaint.setTextSize(25);
+        yellowPaint.setTypeface(tf);
+        // shadow
+        shadowPaint = new Paint(0);
+        shadowPaint.setColor(0xff101010);
+        shadowPaint.setMaskFilter(new BlurMaskFilter(9, BlurMaskFilter.Blur.NORMAL));
+        // linear
+        linearGradientPaint = new Paint();
+        //  linearGradientPaint.setColor(getResources().getColor(R.color.red,null));
+        linearGradientPaint.setStrokeWidth(2);
+
+        // radial
+        radialGradientPaint = new Paint();
+        radialGradientPaint.setColor(Color.BLACK);
+        radialGradientPaint.setStrokeWidth(1);
+        radialGradientPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
     }
 
-    private void setMyFont() {
-        TextPaint textPaint = new TextPaint();
-        Typeface tf =Typeface.createFromAsset(
-                getContext().getAssets(),
-                "fonts/10249.ttf"
-        );
-        textPaint.setTypeface(tf);
-        textPaint.setStrokeWidth(7);
-       // textPaint.setTextSize(Tool.coconvertSpToPx(getContext(), 30));
-        textPaint.setAntiAlias(true);
-        textPaint.setPathEffect(null);
-        textPaint.setColor(getResources().getColor(R.color.green, null));
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
-
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         points.add(new Point((int)event.getX(),(int)event.getY()));
